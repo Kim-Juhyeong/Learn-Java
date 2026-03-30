53bc324f15b4f2a899796c1e06605c347a681e48
Kim-Juhyeong <juhyeong_kim@naver.com>
2026-03-30 11:22:04 +0900

    Ch01 Learning Complete

---
+++ src/ch01/sec12/Calculator.java
@@ -0,0 +1,12 @@
+package ch01.sec12;
+/*
+@author: Kim-Juhyeong
+*/
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(a + " +" + b + " = " + sum);
+    }
+}
